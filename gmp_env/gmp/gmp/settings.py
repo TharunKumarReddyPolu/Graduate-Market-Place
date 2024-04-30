ee0ce7c80af3110cc4e7755a029133e7f44fe2be
Polu Tharun Kumar Reddy <polutharun528@gmail.com>
2024-04-30 12:05:32 -0400

    Added User and admin Flows & whitenoise module

--- gmp_env/gmp/gmp/settings.py
+++ gmp_env/gmp/gmp/settings.py
@@ -33,14 +33,6 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-# Configure email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'graduatemarketplace@gmail.com'
-EMAIL_HOST_PASSWORD = 'Gmarketplace@123'
-
 # Application definition
 
 INSTALLED_APPS = [
